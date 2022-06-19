e34739e10b482efa8747e2fa034abed08db2ab83
0-2HERO <aleksandar1812s@gmail.com>
2022-06-19 14:09:49 +0200

    json server

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,11 +1,29 @@
 <template>
-  <h1>Job Details Page</h1>
+<div v-if="jobs">
+
+  <h1> {{ jobs.title }}</h1>
   <p>The Job id is {{ id }}</p>
+  <p> {{ jobs.details }}</p>
+</div>
+<div v-else>
+    <p>Loading Job details...</p>
+</div>
 </template>
 
 <script>
 export default {
     props: [ 'id'],
+    data() {
+        return {
+            jobs: null
+        }
+    },
+    mounted() {
+    fetch('http://localhost:3000/jobs/' + this.id)
+   .then(res => res.json())
+   .then(data => this.jobs = data)
+   .catch(err => console.log(err.message))
+    }
     /* data() {
         return {
             id: this.$route.params.id
